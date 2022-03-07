2d77b68e62c69740d47c3938f2e9b6de2fac24f8
Alex Alan Nunes <alexalannunes@gmail.com>
2022-03-07 20:44:52 -0300

    feat: icons components

--- src/timer/Timer.tsx
+++ src/timer/Timer.tsx
@@ -1,4 +1,5 @@
 import React, {
+  FC,
   Reducer,
   useCallback,
   useEffect,
@@ -8,6 +9,40 @@ import React, {
 } from "react";
 import styles from "./styles.module.scss";
 import audio from "../assets/Clay-Chime-Thunk.wav";
+
+interface IconProps {
+  size?: number;
+  color?: string;
+}
+
+const PauseIcon: FC<IconProps> = ({ size = 24, color = "#000000" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ height: `${size}px`, width: `${size}px` }}
+    height={24}
+    width={24}
+    viewBox="0 0 24 24"
+    fill={color}
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+  </svg>
+);
+
+const PlayIcon: FC<IconProps> = ({ size = 24, color = "#000000" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ height: `${size}px`, width: `${size}px` }}
+    height={24}
+    width={24}
+    viewBox="0 0 24 24"
+    fill={color}
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z" />
+  </svg>
+);
+
 const toTime = (seconds: number) =>
   new Date(seconds * 1000).toISOString().substr(14, 5);
 
@@ -142,7 +177,7 @@ const Timer: React.FC = () => {
         </div>
       </div>
       <button onClick={handleStart} className={styles.button}>
-        start
+        {started ? <PauseIcon size={50} /> : <PlayIcon size={50} />}
       </button>
       <span>{toTime(seconds)}</span>
       <audio ref={audioRef} src={audio}></audio>
